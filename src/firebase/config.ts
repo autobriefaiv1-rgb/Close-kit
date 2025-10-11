4484b263a8cba0f3088d93f67c0a7f660dd81111
Aria <autobriefaiv1@gmail.com>
2025-10-11 13:21:28 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1937529952-38347",
+  "appId": "1:827314141725:web:fa3166574920038063a4f4",
+  "apiKey": "AIzaSyCAXkPMYWtHWUrzhV_oMZq2Zpd1wLRzJ6w",
+  "authDomain": "studio-1937529952-38347.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "827314141725"
+};
